7143f29ee79f7d24d47b07d71e6f8c121807ba8c
Ojong clinton <sheyenoh123@gmail.com>
2026-01-22 12:20:50 +0100

    added maps

--- pages/contact.php
+++ pages/contact.php
@@ -91,6 +91,18 @@ require_once '../includes/header.php';
     </div>
     <!-- contact areas main end -->
 
+        <div class="google-map-area rts-section-gapTop">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="google-map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3248778.1226535137!2d-86.69566092360928!3d37.327475452900615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8842734c8b1953c9%3A0x771f6f4ec5ccdffc!2sKentucky%2C%20USA!5e0!3m2!1sen!2sbd!4v1741757435755!5m2!1sen!2sbd" width="600" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php
 // Include footer
 require_once '../includes/footer.php';
